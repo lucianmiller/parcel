a50bf019acfd9efce40e208e3b8dfea336619cf2
Lucian Miller <lucian.miller185@gmail.com>
2024-07-11 14:57:54 -0700

    Add form for dimensions of package

---
+++ Parcels/Controllers/ParcelsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Parcels.Models;
+using System.Collections.Generic;
+
+namespace Parcels.Controller
+{
+  public class ParcelsController : Controller
+  {
+    [Route("/price")]
+  }
+}
--- Parcels/Models/Parcel.cs
+++ Parcels/Models/Parcel.cs
@@ -4,6 +4,21 @@ namespace Parcels.Models
 {
   public class Parcel
   {
-    
+    public float UserLength { get; set; }
+    public float UserWidth { get; set; }
+    public float UserHeight { get; set; }
+    public float UserVolume { get; set; }
+
+    public Parcel(float length, float width, float height)
+    {
+      UserLength = length;
+      UserWidth = width;
+      UserHeight = height;
+    }
+
+    public void Volume()
+    {
+      UserVolume = UserLength * UserHeight * UserWidth;
+    }
   }
 }
